0865b67256a6785ef6e5b5409392b169f9399449
aespinos7 <aespinos7@cuc.edu.co>
2024-09-24 13:13:59 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import miLogo from "../src/assets/img/2b938961e605154f41548d01cc87305e.jpg";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={miLogo} alt="Logo React"/>
+        <h1>¡¡¡Hola Muchachos!!!</h1>
+        <p>Este es mi taller React...</p>
+      </div>
+    </>
+  )
+}
+
+export default App
